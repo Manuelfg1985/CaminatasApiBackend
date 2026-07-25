788af39a0f0cfa53cd78fa1e0ffd58b0a7b73b45
Manuelfg1985 <manuelfg2@gmail.com>
2026-07-25 20:16:35 -0300

    Soportar credencial de Firebase vía variable de entorno en producción

--- config/firebase.js
+++ config/firebase.js
@@ -2,9 +2,17 @@ import { initializeApp, cert } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync(new URL("../firebase-service-account.json", import.meta.url))
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Producción (Render): la credencial viene de una variable de entorno
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Desarrollo local: la credencial viene del archivo en disco
+  serviceAccount = JSON.parse(
+    readFileSync(new URL("../firebase-service-account.json", import.meta.url))
+  );
+}
 
 initializeApp({
   credential: cert(serviceAccount),
